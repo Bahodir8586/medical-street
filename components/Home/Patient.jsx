69e6329d0409b654c8a4299b2c6df4d83abd0b53
Bahodir8586 <bahodira213@gmail.com>
2021-11-18 12:30:44 +0500

    slider input created

--- components/Home/Patient.jsx
+++ components/Home/Patient.jsx
@@ -1,15 +1,19 @@
-import SelectQuestion from '@/components/Questions/SelectQuestion/Question';
+import SelectQuestion from '@/components/Questions/SelectQuestion';
+import MultipleChoiceQuestion from '@/components/Questions/MultipleChoiceQuestion';
+import Slider from '@/components/Questions/Slider';
 
 export default function Patient() {
   return (
     <div className="flex flex-col">
-      <SelectQuestion
-        title="What is your sex"
+      {/* <SelectQuestion
+        title="What is your sex?"
         options={[
           { id: 'female', label: 'Female' },
           { id: 'male', label: 'Male' },
         ]}
-      />
+      /> */}
+      <MultipleChoiceQuestion />
+      <Slider title="How old are you?" />
       <div className="border-t flex justify-end items-center py-4">
         <button className="inline-flex items-center px-6 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
           Next
--- components/Questions/Slider/Slider.jsx
+++ components/Questions/Slider/Slider.jsx
@@ -1,3 +1,49 @@
-export default function Slider() {
-  return <></>;
+import { useState } from 'react';
+import { PlusIcon, MinusIcon } from '@heroicons/react/outline';
+
+export default function Slider({ title }) {
+  const [sliderValue, setSliderValue] = useState(25);
+
+  const subtract = () => {
+    if (sliderValue === 0) {
+      return;
+    }
+    setSliderValue(sliderValue - 1);
+  };
+  const add = () => {
+    if (sliderValue === 100) {
+      return;
+    }
+    setSliderValue(sliderValue + 1);
+  };
+  return (
+    <div className="flex flex-col items-center justify-center h-108">
+      <h2 className="text-3xl font-semibold text-center mb-8">{title}</h2>
+      <div className="w-3/4 mx-auto">
+        <div className="text-2xl font-medium ml-12 mb-6">{sliderValue}</div>
+        <div className="flex">
+          <button
+            onClick={subtract}
+            className="p-4 bg-gray-50 rounded-full mr-2 border hover:bg-gray-100 cursor-pointer"
+          >
+            <MinusIcon className="w-6 h-6" />
+          </button>
+          <input
+            className="w-full cursor-pointer"
+            type="range"
+            min={0}
+            max={100}
+            value={sliderValue}
+            onChange={(e) => setSliderValue(e.target.value)}
+          />
+          <button
+            onClick={add}
+            className="p-4 bg-gray-50 rounded-full ml-2 border hover:bg-gray-100 cursor-pointer"
+          >
+            <PlusIcon className="w-6 h-6" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
